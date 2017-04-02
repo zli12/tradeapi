54d90d7c7a63dee88831703d019d0d772fdba724
zli <zli@outlook.com>
2017-04-02 16:17:52 -0400

    Change to Trading API

---
+++ src/controllers/tradeController.js
@@ -0,0 +1,45 @@
+var tradeController = function(Trade) {
+  var post = function(req, res) {
+    var trade = new Trade(req.body);
+
+    if(!trade.symbol) {
+      res.status(400);
+      res.send('Trade symbol is required');
+    }
+    else {
+      trade.save();
+      res.status(201);
+      res.send(trade);
+    }
+  };
+
+  var get = function(req, res) {
+    var query = {};
+
+    if (req.query.symbol) {
+      query.symbol = req.query.symbol;
+    }
+
+    Trade.find(query, function(err, trades) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        var returnedTrades = [];
+        trades.forEach(function(element, index, array){
+          var clonedTrade = element.toJSON();
+          clonedTrade.links = {};
+          clonedTrade.links.self = 'http://' + req.headers.host + '/api/trades/' + clonedTrade._id;
+          returnedTrades.push(clonedTrade);
+        });
+        res.json(returnedTrades);
+      }
+    });
+  };
+
+  return {
+    post: post,
+    get: get
+  };
+};
+
+module.exports = tradeController;
